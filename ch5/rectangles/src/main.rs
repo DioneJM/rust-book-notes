611dd4506891a8f67d068c357150046430dd67f5
Dione <dionemorales@outlook.com>
2021-01-31 22:41:02 +1100

    Added can_hold method for rectangles exercise

--- ch5/rectangles/src/main.rs
+++ ch5/rectangles/src/main.rs
@@ -14,10 +14,18 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.height * self.width
     }
+
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        self.area() > other_rect.area()
+    }
 }
 fn main() {
     println!("Hello, world!");
 
     let rect: Rectangle = Rectangle::new(10, 32);
     println!("Rectangle area: {}", rect.area());
+
+    let big_rect: Rectangle = Rectangle::new(100, 90);
+    let smaller_rect: Rectangle = Rectangle::new(99, 89);
+    println!("Can hold: {}", big_rect.can_hold(&smaller_rect));
 }
